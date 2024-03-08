b2998e844cabfcb18bf56bfb0f50134841d6f6b7
DESCOMPS Théo <tdescomps@bordeaux-inp.fr>
2024-03-08 09:23:19 +0100

    Création du readme.md

--- tracker/serveur.c
+++ tracker/serveur.c
@@ -26,12 +26,12 @@ int main(int argc, char *argv[])
     bzero((char *)&serv_addr, sizeof(serv_addr));
     portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_addr.s_addr = INADDR_ANY; // j'ai pas bien compris ce point (p.11 des slides)
     serv_addr.sin_port = htons(portno);
     if (bind(sockfd, (struct sockaddr *)&serv_addr,
              sizeof(serv_addr)) < 0)
         error("ERROR on binding");
-    listen(sockfd, 5);
+    listen(sockfd, 5); // peut-être modifier la longueur de la queue
     clilen = sizeof(cli_addr);
     newsockfd = accept(sockfd,
                        (struct sockaddr *)&cli_addr,
