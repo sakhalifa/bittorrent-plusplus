4f93e7448b6427aa43522c723811ca44b59532dd
CHANCRIN Youri <Youri.Chancrin@bordeaux-inp.fr>
2024-03-13 11:09:24 +0100

    Started adding commandAnnotation annotation

---
+++ peer/src/main/java/commands/CommandAnnotation.java
@@ -0,0 +1,16 @@
+package commands;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface CommandAnnotation {
+    
+
+
+}
+
+
