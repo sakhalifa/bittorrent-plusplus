2994bf6c33e89447e5db62848e0714d45f808209
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2024-03-14 18:05:37 +0100

    actually implement generic listparser

--- peer/src/main/java/parser/ListParser.java
+++ peer/src/main/java/parser/ListParser.java
@@ -9,14 +9,23 @@ import parser.IParser;
 import parser.exceptions.ParserException;
 import util.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
 public class ListParser<T> implements IParser<List<T>> {
 
+	private final Function<String, Pair<T, Integer>> consumerFunction;
+
 	@Override
 	public List<T> parse(String input) throws ParserException {
-		return null;
+		List<T> toReturn = new ArrayList<>();
+		while(!input.isEmpty()){
+			var result = consumerFunction.apply(input);
+			toReturn.add(result.getFirst());
+			input = input.substring(result.getSecond()).trim();
+		}
+		return toReturn;
 	}
 
 	public ListParser(Function<String, Pair<T, Integer>> consumerFunc){
@@ -25,5 +34,6 @@ public class ListParser<T> implements IParser<List<T>> {
 		// and compacting it into a single object.
 		// the given function should take the current input, and return the built object and how many characters it consumed
 		// so the list parser can actually shift the input for the next entry in the list
+		this.consumerFunction = consumerFunc;
 	}
 }
