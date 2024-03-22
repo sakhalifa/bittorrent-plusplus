6a467807e3a9655e48d9cb22ff3c3c12ff905553
Alexandre Tabouret <Alexandre.Tabouret@bordeaux-inp.fr>
2024-03-22 08:21:58 +0100

    fixed h files inckude

--- tracker/command.h
+++ tracker/command.h
@@ -1,9 +1,6 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
-#include <stdlib.h>
-#include <stdio.h>
-
 
 void announce_listen(int port /*, struct file files[], char * keys[]*/);
 
--- tracker/parser.h
+++ tracker/parser.h
@@ -1,11 +1,6 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-
 void parsing(char * command);
 
 #endif
