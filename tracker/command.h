6455e4ac9f43af1a9d95af4fa4f038d1b406c4d1
Alexandre Tabouret <Alexandre.Tabouret@bordeaux-inp.fr>
2024-03-23 14:40:42 +0100

    parsing announce_listen done

--- tracker/command.h
+++ tracker/command.h
@@ -1,13 +1,14 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+#include "file.h"
 
-void announce_listen(int port /*, struct file files[], char * keys[]*/);
+void announce_listen(int port , struct file ** files, int size_file, char ** list_key, int size_keys);
 
 void look();
 
 void getfile(char * key);
 
-void update();
+void update(char ** list_keys, int size);
 
 #endif
--- tracker/parser.c
+++ tracker/parser.c
@@ -16,9 +16,9 @@ void error_command(char *error)
     exit(1);
 }
 
-void string_to_list(char *string, char **list, int *size, const char *separator)
+void string_to_list_key(char *string, char **list, int *size, const char *separator)
 {
-    char * key = strtok(string, separator);
+    char *key = strtok(string, separator);
     while (key != NULL)
     {
         *size += 1;
@@ -29,18 +29,85 @@ void string_to_list(char *string, char **list, int *size, const char *separator)
     }
 }
 
+void string_to_list_file(char *string, struct file *files[], int *size, const char *separator)
+{
+    char *file = strtok(string, separator); // Filename
+    while (file != NULL)
+    {
+        *size += 1;
+        files = realloc(files, *size * sizeof(struct file *));
+        struct file *elmt = malloc(sizeof(struct file));
+        elmt->name = strdup(file);  // Copy name
+        elmt->filesize = atoi(strtok(NULL, separator)); // Copy filesize
+        elmt->piecesize = atoi(strtok(NULL, separator)); // Copy piecesize
+        elmt->key = strdup(strtok(NULL, separator)); // Copy key
+        files[*size - 1] = elmt; // store the file inside the list
+
+        file = strtok(NULL, separator);
+    }
+}
+
 void parsing(char *command)
 {
     const char *separator = " "; // Separator in command is blank " "
+    char *right_bracket_separator = "]";
+    char *left_bracket_separator = "[";
     char *command_name = strtok(command, separator);
 
     // ANNOUNCE command (connection command to announce owned and leeched files)
     if (strcmp(command_name, "announce") == 0)
     {
-        strtok(NULL, separator); // get rid of "listen"
+        // Check if next word is "listen", otherwise error
+        if (strcmp(strtok(NULL, separator), "listen") != 0)
+        {
+            error_command(NULL);
+        }
         int port = atoi(strtok(NULL, separator));
 
-        printf("port %d\n", port);
+
+        // Check if next word is "seed", otherwise error
+        if (strcmp(strtok(NULL, separator), "seed") != 0)
+        {
+            error_command(NULL);
+        }
+
+        char *string_files = strtok(NULL, right_bracket_separator);
+
+        // Check left Bracket for files
+        if (string_files[0] != '[')
+        {
+            error_command("Missing bracket");
+        }
+
+        // Check if next word is "leech", otherwise error
+        if (strcmp(strtok(NULL, separator), "leech") != 0)
+        {
+            error_command(NULL);
+        }
+
+        char *leech_key = strtok(NULL, right_bracket_separator);
+
+        // Check left Bracket for leech
+        if (leech_key[0] != '[')
+        {
+            error_command("Missing bracket");
+        }
+
+        string_files = strtok(string_files, left_bracket_separator);
+        leech_key = strtok(leech_key, left_bracket_separator);
+
+        // Get owned files
+        struct file **files = malloc(sizeof(struct file *));
+        int size_file = 0;
+        string_to_list_file(string_files, files, &size_file, separator);
+
+        // Get leeched keys
+        char **keys = malloc(sizeof(char *)); // TO BE FREED (and its elements)
+        int size_key = 0;
+        string_to_list_key(leech_key, keys, &size_key, separator);
+
+        // announce_listen(port, files, size_file, keys, size_key);
+
     }
 
     // LOOK command (search files according to specific criteria)
@@ -55,20 +122,16 @@ void parsing(char *command)
         char *key = strtok(NULL, separator);
 
         // getfile(key);
-        printf("key : %s\n", key);
     }
 
     // UPDATE command (update self seeded and leeched files)
     else if (strcmp(command_name, "update") == 0)
     {
-        // Check command word "seed"
+        // Check if next word is "seed", otherwise error
         if (strcmp(strtok(NULL, separator), "seed") != 0)
         {
             error_command(NULL);
         }
-
-        char *right_bracket_separator = "]";
-        char *left_bracket_separator = "[";
 
         char *seed_key = strtok(NULL, right_bracket_separator);
 
@@ -78,7 +141,7 @@ void parsing(char *command)
             error_command("Missing bracket");
         }
 
-        // Check command word "leech"
+        // Check if next word is "leech", otherwise error
         if (strcmp(strtok(NULL, separator), "leech") != 0)
         {
             error_command(NULL);
@@ -94,18 +157,21 @@ void parsing(char *command)
 
         seed_key = strtok(seed_key, left_bracket_separator);
         leech_key = strtok(leech_key, left_bracket_separator);
-        printf("seed %s, leech %s\n", seed_key, leech_key);
 
+        // Get owned keys
         char **list = malloc(sizeof(char *)); // TO BE FREED (and its elements)
         int size = 0;
-        string_to_list(seed_key, list, &size, separator);
+        string_to_list_key(seed_key, list, &size, separator);
+
+        // Get leeched keys
         char **list2 = malloc(sizeof(char *)); // TO BE FREED (and its elements)
         int size2 = 0;
-        string_to_list(leech_key, list2, &size2, separator);
+        string_to_list_key(leech_key, list2, &size2, separator);
 
         // Append list2 to list
         list = realloc(list, (size + size2) * sizeof(char *));
-        for (int i = 0; i < size2; i++){
+        for (int i = 0; i < size2; i++)
+        {
             list[size + i] = malloc(strlen(list2[i]) + 1);
             strcpy(list[size + i], list2[i]);
             free(list2[i]);
@@ -113,8 +179,7 @@ void parsing(char *command)
         free(list2);
         size += size2;
 
-        // list contains all keys and has a size of 'size'
-
+        // update(list, size);
     }
 
     // Unknown command
@@ -126,6 +191,6 @@ void parsing(char *command)
 
 int main()
 {
-    char f[] = "update seed [az az5 868a] leech [56az 6azd hthjjoi]";
+    char f[] = "announce listen 1896 seed [jojo 8 7 ae jojo2 9 10 eeee] leech [aad azd ed]";
     parsing(f);
 }
