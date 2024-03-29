eb45cdd4ecccb493da322d02f4e3081ecf854e74
CHANCRIN Youri <Youri.Chancrin@bordeaux-inp.fr>
2024-03-29 10:12:20 +0100

    Implemented getPartition for files

---
+++ peer/src/test/java/fr/ystat/files/FileInventoryTest.java
@@ -0,0 +1,40 @@
+package fr.ystat.files;
+
+
+import org.junit.jupiter.api.Test;
+
+public class FileInventoryTest {
+
+    // Default test
+    @Test
+    public void instantiationTest() {
+        FileInventory fileInventory =  FileInventory.getInstance();
+    }
+
+    @Test
+    public void fileAdditionTest() {
+        FileInventory fileInventory =  FileInventory.getInstance();
+        fileInventory.addStockedFile(new DownloadedFile("File1", 10000, 2000, "fakeHash1"));
+        fileInventory.addStockedFile(new DownloadedFile("File2", 100000, 2000, "fakeHash2"));
+        fileInventory.addStockedFile(new DownloadedFile("File3", 50000, 2000, "fakeHash3"));
+        fileInventory.addStockedFile(new DownloadedFile("File4", 20000, 2000, "fakeHash4"));
+    }
+
+    @Test
+    public void fileRequestTest() {
+        FileInventory fileInventory =  FileInventory.getInstance();
+        StockedFile f1 = new DownloadedFile("File1", 10000, 2000, "fakeHash1");
+        StockedFile f2 = new DownloadedFile("File2", 100000, 2000, "fakeHash2");
+        StockedFile f3 = new DownloadedFile("File3", 50000, 2000, "fakeHash3");
+        StockedFile f4 = new DownloadedFile("File4", 20000, 2000, "fakeHash4");
+        fileInventory.addStockedFile(f1);
+        fileInventory.addStockedFile(f2);
+        fileInventory.addStockedFile(f3);
+        fileInventory.addStockedFile(f4);
+
+        assert fileInventory.getStockedFile("fakeHash1") == f1;
+        assert fileInventory.getStockedFile("fakeHash2") == f2;
+        assert fileInventory.getStockedFile("fakeHash3") == f3;
+        assert fileInventory.getStockedFile("fakeHash4") == f4;
+    }
+}
