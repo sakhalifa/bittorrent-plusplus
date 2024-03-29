b3e3e45c9874bf247be95fae36c11b74173d2553
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2024-03-29 10:11:14 +0100

    implement getpieces and interested parser

---
+++ peer/src/main/java/fr/ystat/commands/server/GetPiecesCommand.java
@@ -0,0 +1,59 @@
+package fr.ystat.commands.server;
+
+import fr.ystat.commands.CommandAnnotation;
+import fr.ystat.commands.ICommand;
+import fr.ystat.commands.ICommandParser;
+import fr.ystat.commands.exceptions.CommandException;
+import fr.ystat.parser.ListParser;
+import fr.ystat.parser.exceptions.InvalidInputException;
+import fr.ystat.parser.exceptions.ParserException;
+import fr.ystat.server.Counter;
+import fr.ystat.util.Pair;
+
+import java.util.Arrays;
+import java.util.List;
+
+class GetPiecesParser implements ICommandParser{
+
+	@Override
+	public ICommand parse(String input) throws ParserException {
+		String[] splitted = input.split(" ");
+		if (splitted.length < 3) {
+			throw new InvalidInputException(input);
+		}
+		System.out.println(input.matches("getpieces [a-z0-9]{32} \\[[0-9]+( [0-9]+)*]"));
+		if(!input.matches("getpieces [a-z0-9]{32} \\[[0-9]+( [0-9]+)*]")){
+			throw new InvalidInputException(input);
+		}
+		ListParser<Integer> indexListParser = new ListParser<>((integerList, idx) -> {
+			try{
+				int n = Integer.parseInt(integerList[idx]);
+				return new Pair<>(n, 1);
+			}catch(NumberFormatException ex){
+				throw new InvalidInputException(String.join(" ", integerList));
+			}
+		});
+		List<Integer> indexList = indexListParser.parse(input.substring("getpieces".length() + 32 + 2 + 1, input.length() - 1));
+		return new GetPiecesCommand(splitted[1], indexList);
+	}
+}
+
+@CommandAnnotation(name = "getpieces", parser = GetPiecesParser.class)
+public class GetPiecesCommand implements ICommand{
+	private final String key;
+	private final List<Integer> indexes;
+
+	public GetPiecesCommand(String key, List<Integer> indexes){
+		this.key = key;
+		this.indexes = indexes;
+	}
+	@Override
+	public String apply(Counter counter) throws CommandException {
+		return this.key + " " + this.indexes;
+	}
+
+	@Override
+	public String serialize() {
+		return ICommand.super.serialize();
+	}
+}
