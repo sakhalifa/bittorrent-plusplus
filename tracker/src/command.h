3bcfea12ae3e63ea0783d92ded2ac6deaedf333b
Alexandre Tabouret <Alexandre.Tabouret@bordeaux-inp.fr>
2024-04-15 10:03:43 +0200

    small fix parser.h and command.h

--- tracker/src/command.h
+++ tracker/src/command.h
@@ -3,13 +3,53 @@
 
 #include "file.h"
 
-void announce_listen(int port, struct file **files, int size_file,
-    char **list_key, int size_keys);
+enum comparator { LT = -1, EQ = 0, GT = 1 };
 
-void look();
+struct criteria {
+	char *element; // Name of the criteria
+	enum comparator comp; // Comparator
+	char *value; // value to compare to
+};
 
-void getfile(char *key);
+enum command_num {
+	ANNOUNNCE,
+	LOOK,
+	GETFILE,
+	UPDATE,
+};
 
-void update(char **list_keys, int size);
+struct command {
+	enum command_num command_name;
+	void *command_arg;
+};
+
+struct announce {
+	int port;
+	int nb_file;
+	struct file **file_list;
+	int nb_key;
+	char **key_list;
+};
+
+struct look {
+	struct criteria **criteria;
+};
+
+struct getfile {
+	char *key;
+};
+
+struct update {
+	int nb_key;
+	char **key_list;
+};
+
+void announce(struct command command, struct file files[], struct peer peer);
+
+void look(struct command command, struct file files[], struct peer peer);
+
+void getfile(struct command command, struct file files[], struct peer peer);
+
+void update(struct command command, struct file files[], struct peer peer);
 
 #endif
