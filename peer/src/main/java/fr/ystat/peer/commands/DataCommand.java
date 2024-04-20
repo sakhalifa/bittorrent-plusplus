31922e766255c6b827eef0808a1bd5932a58f73b
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2024-04-20 18:09:20 +0200

    rework DataCommand to make it work on all possible string of characters, as it is just a binary representation

--- peer/src/main/java/fr/ystat/peer/commands/DataCommand.java
+++ peer/src/main/java/fr/ystat/peer/commands/DataCommand.java
@@ -4,12 +4,18 @@ import fr.ystat.commands.CommandAnnotation;
 import fr.ystat.commands.ICommandParser;
 import fr.ystat.commands.IReceivableCommand;
 import fr.ystat.commands.exceptions.CommandException;
+import fr.ystat.files.FileInventory;
+import fr.ystat.files.StockedFile;
 import fr.ystat.parser.ListParser;
 import fr.ystat.parser.ParserUtils;
 import fr.ystat.parser.exceptions.InvalidInputException;
 import fr.ystat.parser.exceptions.ParserException;
 import fr.ystat.util.Pair;
+import lombok.Getter;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,38 +24,91 @@ import java.util.stream.Collectors;
 
 class DataParser implements ICommandParser {
 
-    @Override
-    public IReceivableCommand parse(String input) throws ParserException {
+	@Override
+	public IReceivableCommand parse(String input) throws ParserException {
 
-        String[] splitted = ParserUtils.expectArgs(input, 3, "data");
+		String[] splitted = ParserUtils.expectArgs(input, 3, "data");
 
-        String fileHash = ParserUtils.parseKeyCheck(splitted[1]);
+		String fileHash = ParserUtils.parseKeyCheck(splitted[1]);
 
-        // Parse Map of id:data :/
-        List<Pair<Integer, String>> dataList = new ListParser<>((lst, idx) -> {
-            String[] split = lst[idx].split(":");
-            if (split.length != 2) throw new InvalidInputException(input, "data.badElFormat." + idx);
-            int buffer_index = Integer.parseInt(split[0]);
-            String index_data = split[1];
-            return new Pair<>(new Pair<>(buffer_index, index_data), 1);
-        }).parse(input.substring(splitted[0].length() + 1 + fileHash.length() + 1 + 1, input.length() - 1));
-        return new DataCommand(dataList);
-    }
+		StockedFile file = FileInventory.getInstance().getStockedFile(fileHash);
+		if (file == null)
+			throw new InvalidInputException("[data.noSuchFile] File not found");
+
+		long dataLen = file.getProperties().getPieceSize();
+		List<Map.Entry<Integer, ByteBuffer>> dataList = new ArrayList<>();
+		int startIdx = splitted[0].length() + 1 + splitted[1].length() + 1 + 1;
+		try {
+			String toRead = input.substring(startIdx);
+			while (!toRead.isEmpty()) {
+				int firstSepIdx = toRead.indexOf(':');
+				if(firstSepIdx == -1)
+					throw new InvalidInputException("[data.badFormat.noDataSep] bad format");
+				String pieceNumStr = toRead.substring(0, firstSepIdx);
+				int pieceNum;
+				try{
+					pieceNum = Integer.parseInt(pieceNumStr);
+				}catch(NumberFormatException e){
+					throw new InvalidInputException("[data.badFormat.pieceNumNotANum] bad format");
+				}
+				String data = toRead.substring(firstSepIdx+1, (int) (firstSepIdx+1+dataLen));
+				dataList.add(Map.entry(pieceNum, StandardCharsets.ISO_8859_1.encode(data)));
+				toRead = toRead.substring((int) (firstSepIdx+1+dataLen+1));
+			}
+		}catch (StringIndexOutOfBoundsException e){
+			throw new InvalidInputException("[data.badFormat] bad format");
+		}
+
+//		List<Map.Entry<Integer, ByteBuffer>> dataList = new ListParser<>((lst, idx) -> {
+//			System.out.printf("[%d] %s%n", idx, lst[idx]);
+//			String[] split = lst[idx].split(":");
+//			int firstSeparator = lst[idx].lastIndexOf(':');
+//			if(firstSeparator == -1) // cannot check splitted.length due to edge case "1::::::::" which returns {"1"}
+//				throw new InvalidInputException("[data.badFormat] Invalid format");
+//			String pieceNumStr = split[0];
+//			int pieceNum;
+//			try{
+//				pieceNum = Integer.parseInt(pieceNumStr);
+//			}catch(NumberFormatException e){
+//				throw new InvalidInputException("[data.badFormat] Invalid format");
+//			}
+//			StringBuilder reading = new StringBuilder();
+//			for(int i = 1; i<split.length; ++i){
+//				reading.append(split[i]);
+//				reading.append(":");
+//			}
+//			reading.deleteCharAt(reading.length()-1);
+//			int off = 1;
+//			while(reading.length() != file.getProperties().getPieceSize() && idx+off < lst.length){
+//				reading.append(' ');
+//				String str = lst[idx + off];
+//				reading.append(str);
+//				++off;
+//			}
+//			while(reading.length() != file.getProperties().getPieceSize())
+//				reading.append(' '); // EDGE CASE I LOVE IT YAY
+//			return new Pair<>(Map.entry(pieceNum, StandardCharsets.ISO_8859_1.encode(reading.toString())), off);
+//		}).parse(input.substring(splitted[0].length() + 1 + splitted[1].length() + 1 + 1, input.length() - 1));
+		return new DataCommand(dataList);
+	}
 }
 
 
+@Getter
 @CommandAnnotation(name = "data", parser = DataParser.class)
 public class DataCommand implements IReceivableCommand {
-    private final Map<Integer, String> dataList;
+	private final Map<Integer, ByteBuffer> dataList;
 
-    public DataCommand(List<Pair<Integer, String>> dataList) throws IllegalArgumentException {
-        // Throws if duplicates keys are present
-        this.dataList = new HashMap<>(dataList.stream().collect(Collectors.toMap(Pair::getFirst, Pair::getSecond)));
-    }
-    @Override
-    public String apply() throws CommandException {
+	public DataCommand(List<Map.Entry<Integer, ByteBuffer>> dataList) throws IllegalArgumentException {
+		this.dataList = dataList
+				.stream()
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+	}
+
+	@Override
+	public String apply() throws CommandException {
 
 
-        return "";  // You don't want to send it, don't you ? :3
-    }
+		return "";  // You don't want to send it, don't you ? :3
+	}
 }
