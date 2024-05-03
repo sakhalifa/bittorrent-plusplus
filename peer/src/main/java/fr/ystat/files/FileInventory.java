2f6423b778d584bd6bd0a5cce645d6a445e124ff
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2024-05-03 08:21:45 +0200

    update fileinventory to add a list of listeners

--- peer/src/main/java/fr/ystat/files/FileInventory.java
+++ peer/src/main/java/fr/ystat/files/FileInventory.java
@@ -1,9 +1,14 @@
 package fr.ystat.files;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Consumer;
 
 public class FileInventory {
 
@@ -11,6 +16,8 @@ public class FileInventory {
     final private static Object mutex = new Object();
 
     final private ConcurrentMap<String, StockedFile> filesMap = new ConcurrentHashMap<>();
+	private final Queue<Consumer<StockedFile>> fileAddedListeners = new ConcurrentLinkedQueue<>();
+	private final Queue<Consumer<StockedFile>> fileRemovedListeners = new ConcurrentLinkedQueue<>();
 
     private FileInventory() {}
 
@@ -27,9 +34,29 @@ public class FileInventory {
 		return result;
 	}
 
+	public boolean contains(String hash){
+		return filesMap.containsKey(hash);
+	}
+
     public void addStockedFile(StockedFile newFile) {
         filesMap.put(newFile.getProperties().getHash(), newFile);
+		for(var listener : fileAddedListeners) {
+			listener.accept(newFile);
+		}
     }
+
+	public boolean removeStockedFile(String hash) {
+		StockedFile removed = filesMap.remove(hash);
+		if(removed != null)
+			for(var listener : fileRemovedListeners) {
+				listener.accept(removed);
+			}
+		return removed != null;
+	}
+
+	public boolean removeStockedFile(StockedFile stockedFile) {
+		return removeStockedFile(stockedFile.getProperties().getHash());
+	}
 
     public StockedFile getStockedFile(String hash) {
         return filesMap.get(hash);
@@ -37,5 +64,21 @@ public class FileInventory {
 
 	public Collection<StockedFile> getAllFiles() {
 		return filesMap.values();
+	}
+
+	public void addOnFileAddedListener(Consumer<StockedFile> listener){
+		fileAddedListeners.add(listener);
+	}
+
+	public void removeOnFileAddedListener(Consumer<StockedFile> listener){
+		fileAddedListeners.remove(listener);
+	}
+
+	public void addOnFileRemovedListener(Consumer<StockedFile> listener){
+		fileRemovedListeners.add(listener);
+	}
+
+	public void removeOnFileRemovedListener(Consumer<StockedFile> listener){
+		fileRemovedListeners.remove(listener);
 	}
 }
