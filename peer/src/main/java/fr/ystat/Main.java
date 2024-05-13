2788a1033adff6245de5feda8b25599929a2e489
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2024-05-13 09:38:58 +0200

    remove the "./"

--- peer/src/main/java/fr/ystat/Main.java
+++ peer/src/main/java/fr/ystat/Main.java
@@ -105,7 +105,7 @@ public class Main {
 		validateConfiguration(configurationManager);
 		for (String arg : args) {
 			FileInventory.getInstance().addStockedFile(
-					CompletedFile.fromLocalFile(new File("./" + arg), 2048L)
+					CompletedFile.fromLocalFile(new File(arg), 2048L)
 			);
 		}
 		seeder = new Seeder();
